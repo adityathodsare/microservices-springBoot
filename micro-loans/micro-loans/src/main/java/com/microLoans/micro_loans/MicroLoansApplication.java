a33b3acb98381e8df4cccf8f4b95ba2e1aa1c2ff
adityathodsare <thodsareaditya@gmail.com>
2025-06-09 10:55:36 +0530

    added new project as microservices for loan as microLoan

---
+++ micro-loans/micro-loans/src/main/java/com/microLoans/micro_loans/MicroLoansApplication.java
@@ -0,0 +1,13 @@
+package com.microLoans.micro_loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroLoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroLoansApplication.class, args);
+	}
+
+}
